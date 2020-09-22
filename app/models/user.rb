a7ea7c95290dcc107ee33b8d34bcd9c6a09985ba
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2020-09-23 03:55:08 +0900

    ユーザー登録実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+with_options presence: true, format: { with: /\A[一-龥]+\z/ } do
+  validates :first_name_kanji
+  validates :family_name_kanji
+end
+with_options presence: true, format: { with: /\A[ァ-ン]+\z/ } do
+  validates :first_name_kana
+  validates :family_name_kana
+end
+
+validates :email, format: { with: /@.+/ } 
+
+validates :password,    length: { minimum: 6 }   
+
 end

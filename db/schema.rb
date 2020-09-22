5ca3d2883fe36d6d1ce0ed2b6dcce8a5c93bd4ca
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2020-09-22 23:19:56 +0900

    userテーブルカラム変更

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,12 @@ ActiveRecord::Schema.define(version: 2020_09_21_163830) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "nickname", default: "", null: false
+    t.string "family_name_kanji", default: "", null: false
+    t.string "first_name_kanji", default: "", null: false
+    t.string "family_name_kana", default: "", null: false
+    t.string "first_name_kana", default: "", null: false
+    t.date "birth_day", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"

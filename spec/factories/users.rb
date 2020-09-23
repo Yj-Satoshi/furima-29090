a79266c0d6eddd5848870fe69edbb05016a14064
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2020-09-24 03:30:45 +0900

    factory_botファイル名修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,9 @@ FactoryBot.define do
     factory :user do
         nickname {Faker::Name.initials(number: 2)}
         email {Faker::Internet.free_email}
-        password = Faker::Internet.password(mini_length: 6)
+        password = Faker::Internet.password(min_length: 6)
         password {password}
-        password_comfirmation {password}
+        password_confirmation {password}
         birth_day {Faker::Date.in_date_period}
         family_name_kanji {"山田"}
         first_name_kanji {"陸郎"}

e5208c0bc748b958aeccd7f7ecc3b8c814e45382
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2020-09-26 15:24:43 +0900

    migrateファイルカラム設定

--- db/migrate/20200925160506_create_items.rb
+++ db/migrate/20200925160506_create_items.rb
@@ -1,7 +1,15 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-
+      t.refarences :user        ,foreign_ky: true
+      t.string :name            ,null: false, default:""
+      t.text :explanation       ,null: false, default:""
+      t.integer :price          ,null: false
+      t.integer :category_id    ,null: false
+      t.integer :item_status_id ,null: false
+      t.integer :send_area_id   ,null: false
+      t.integer :send_date_id   ,null: false
+      t.integer :send_fee_id    ,null: false
       t.timestamps
     end
   end

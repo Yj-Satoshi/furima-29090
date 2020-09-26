a9c00ac324f829859ed5084daba31f7d03f08b9e
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2020-09-26 15:42:59 +0900

    バリデーション(itemモデル)、アソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+
+    with_options presence: true do
+        validates :name, :explanation, :price, :category_id, :item_status_id, :send_area_id, :send_date_id, :send_fee_id 
+    end
+
+    belongs_to :user
 end

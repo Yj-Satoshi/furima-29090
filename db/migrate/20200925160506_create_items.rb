b60f4af9d254d317748cfb7d6d4ac3838a2cac05
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2020-09-26 15:56:18 +0900

    migrateファイル修正

--- db/migrate/20200925160506_create_items.rb
+++ db/migrate/20200925160506_create_items.rb
@@ -1,9 +1,9 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.refarences :user        ,foreign_ky: true
+      t.references :user        ,foreign_ky: true
       t.string :name            ,null: false, default:""
-      t.text :explanation       ,null: false, default:""
+      t.text :explanation       ,null: false
       t.integer :price          ,null: false
       t.integer :category_id    ,null: false
       t.integer :item_status_id ,null: false

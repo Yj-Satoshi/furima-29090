30eab5f57582fe9ba17c23dc0e52559653d8f651
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2020-09-26 18:56:03 +0900

    アソシエーション、バリデーション設定
    ActiveHashのitemモデルのアソシエーション、バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,4 +6,15 @@ class Item < ApplicationRecord
 
     belongs_to :user
     has_one_attached :image
+
+    extend ActiveHash::associations::ActiveRecordExtensions
+    belongs_to_active_hash :category
+    belongs_to_active_hash :item_status
+    belongs_to_active_hash :send_area
+    belongs_to_active_hash :send_date
+    belongs_to_active_hash :send_fee
+
+    with_options numericality: { other_than: 1} do
+        validates :category_id, :item_status_id, :send_area_id, :send_date_id, :send_fee_id
+    end
 end

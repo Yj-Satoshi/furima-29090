3f9690d574fa2c630f71f49d5b1023de32a98afe
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2020-09-29 06:02:19 +0900

    Faker実装（itemモデル）

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :item do
-    
+    name { Faker::Teame.name }
+    explanation { Faker::Lorem.sentence }
+    price { Faker::Number.within(range: 300..9999999) }
+    category_id { Faker::Number.digit }
+    send_area_id { Faker::Number.digit }
+    send_date_id { Faker::Number.digit }
+    send_fee_id { Faker::Number.digit }
   end
 end

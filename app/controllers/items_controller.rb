50741ea4be6ac84895cf18bbd35e4fa8d987f0f9
Yj-Satoshi <65821688+Yj-Satoshi@users.noreply.github.com>
2020-10-01 19:11:50 +0900

    itemsコントローラー修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,6 @@
 class ItemsController < ApplicationController
     before_action :move_to_sign_in, except: [:index, :show, :search]
 
-    def index
-        @items = Item.includes(:user)
-    end
-
     def new
         @item = Item.new
     end
